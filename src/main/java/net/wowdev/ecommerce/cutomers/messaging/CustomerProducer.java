a99810398d989ad0903f30b87a7e90c2e8127284
Wagner O. Wutzke <wagner.wutzke@google.com>
2026-09-03 23:13:46 -0300

    Experimenting with @TransactionalEventListener

--- src/main/java/net/wowdev/ecommerce/cutomers/messaging/CustomerProducer.java
+++ src/main/java/net/wowdev/ecommerce/cutomers/messaging/CustomerProducer.java
@@ -5,6 +5,8 @@ import net.wowdev.ecommerce.domain.events.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
@@ -20,21 +22,25 @@ public class CustomerProducer {
     this.customerEventsTopic = customerEventsTopic;
   }
 
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
   public void publish(final CustomerLoadedEvent event) {
     log.debug(">> Publishing CustomerLoadedEvent: {}", event.eventId());
     template.send(customerEventsTopic, event.transactionId(), event);
   }
 
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
   public void publish(final CustomerLoadingFailedEvent event) {
     log.debug(">> Publishing CustomerLoadingFailedEvent: {}", event.eventId());
     template.send(customerEventsTopic, event.transactionId(), event);
   }
 
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
   public void publish(final PaymentMethodLoadedEvent event) {
     log.debug(">> Publishing PaymentMethodLoadedEvent: {}", event.eventId());
     template.send(customerEventsTopic, event.transactionId(), event);
   }
 
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
   public void publish(OrderProcessingStartedEvent event) {
     log.debug(">> Publishing OrderProcessingStartedEvent: {}", event.eventId());
     template.send(customerEventsTopic, event.transactionId(), event);
