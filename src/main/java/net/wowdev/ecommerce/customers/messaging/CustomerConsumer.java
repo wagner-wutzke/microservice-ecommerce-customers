d98c6e26db89b44e78a0f9414d3e33e270cd7483
Wagner O. Wutzke <wagner.wutzke@google.com>
2026-09-08 22:50:16 -0300

    Adjustments for SAGA testing

--- src/main/java/net/wowdev/ecommerce/customers/messaging/CustomerConsumer.java
+++ src/main/java/net/wowdev/ecommerce/customers/messaging/CustomerConsumer.java
@@ -28,6 +28,6 @@ public class CustomerConsumer {
 
   @KafkaHandler(isDefault = true)
   public void handleUnknown(Object event) {
-    log.debug(">> Received an unmapped event of type {}", event.getClass().getSimpleName());
+    //log.debug(">> Received an unmapped event of type {}", event.getClass().getSimpleName());
   }
 }
